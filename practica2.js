dde5d15946f8d748051747b5a859ee75a491ca02
aslow01 <matias.ariel.ormeno202@gmail.com>
2023-02-28 05:34:08 -0300

    es una prueba

---
+++ practica2.js
@@ -0,0 +1,4 @@
+function tienenElMismoCaracter(str1, str2){
+    return str1.length === str2.length
+    }
+    console.log("hola" +" "+ "nombre");
---
+++ practicas.js
@@ -0,0 +1,13 @@
+function puedeManejar(edad) {
+  if(edad >= 18) {
+           console.log( true );
+  } 
+  console.log( false );
+}
+
+puedeManejar(20);
+
+
+function tienenElMismoCaracter(str1, str2){
+return str1.length === str2.length
+}
